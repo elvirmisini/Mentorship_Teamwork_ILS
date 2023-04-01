8a8d8d0ec1131ee634ee615dfe572013e84d2171
uranlajci <117693854+uran-lajci@users.noreply.github.com>
2023-04-01 18:03:13 +0200

    added the solution and the output files for initial_solution_with_random_project_sort

--- initial_solution_with_random_project_sort.py
+++ initial_solution_with_random_project_sort.py
@@ -60,14 +60,41 @@ def get_possible_combinations_of_project_and_contributors(projects, possible_com
         number_of_project_skills = len(projects_as_dict[possible_combination]["skills"])
         new_possible_combination_of_project_with_contributors[possible_combination] = possible_combination_of_project_with_contributors[possible_combination][0:number_of_project_skills]
     
-    return new_possible_combination_of_project_with_contributors
+    return new_possible_combination_of_project_with_contributors, projects_as_dict
+
+
+def get_only_the_correct_groups(combination_of_projects_and_contributors, projects_as_dict, contributors):
+    correct_combination = {}
+    for project_name, project_contributors in combination_of_projects_and_contributors.items():
+        list_of_contributors = set()
+        project_skills = projects_as_dict[project_name]["skills"]
+        for project_skill in project_skills:
+            pr_skill = list(project_skill.keys())[0]
+            pr_level = project_skill[pr_skill]
+
+            for project_contributor in project_contributors:
+                contributor_skills = contributors[project_contributor]
+                for contributor_skill, contributor_level in contributor_skills.items():
+                    if contributor_skill == pr_skill and contributor_level >= pr_level:
+                        list_of_contributors.add(project_contributor)
+                        # print(project_name, "-", project_contributor, " - ", contributor_skill, contributor_level, " - ", pr_skill, pr_level)
+                        
+        correct_combination[project_name] = list_of_contributors
+    
+    projects_and_contributors = {}
+    for combination in correct_combination:
+        if correct_combination[combination] != set():
+            projects_and_contributors[combination] = correct_combination[combination]
+    
+    return projects_and_contributors
 
 if __name__ == "__main__":
     number_of_contributors, number_of_projects, contributors, projects = read_contributors_and_projects(input_file)
+    # print_contributors_and_projects_info(number_of_contributors, number_of_projects, contributors, projects)
     random.shuffle(projects)
 
-    combination = get_possible_combinations_of_project_and_contributors(projects, get_possible_combinations_of_projects_and_contributors(projects, contributors))
-    print("\n", combination, "\n")
+    combinations, projects_as_dict = get_possible_combinations_of_project_and_contributors(projects, get_possible_combinations_of_projects_and_contributors(projects, contributors))
+    # print("\n", combinations, "\n")
 
     # from the combinaiton we get something like this
     # {'WebChat': ['Bob', 'Maria'], 'Logging': ['Anna'], 'WebServer': ['Bob', 'Anna']} 
@@ -75,3 +102,19 @@ if __name__ == "__main__":
     # for example this combinations 'WebChat': ['Bob', 'Maria'], 'WebServer': ['Bob', 'Anna'] are correct 
     # these combination 'Logging': ['Anna'] is not correct 
     # so we save only the correct combinations in the output file
+
+    x = get_only_the_correct_groups(combinations, projects_as_dict, contributors)
+    for key in x:
+        x[key] = list(x[key])
+    
+    correct = {}
+    for project in projects:
+        for x_name in x:
+            if x_name == project["name"]:
+                nr_contributors = len(x[x_name])
+                nr_con_in_project = len(project["skills"])
+                if nr_con_in_project == nr_contributors:
+                    correct[x_name] = x[x_name]
+
+    save_assignments(correct, output_file)
+    
