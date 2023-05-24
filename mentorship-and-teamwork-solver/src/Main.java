06c6c59b75d2709e45da39acaa78a46b4d3b0327
elvirmisini <elvirmisini@gmail.com>
2023-05-25 00:24:14 +0200

    main

--- mentorship-and-teamwork-solver/src/Main.java
+++ mentorship-and-teamwork-solver/src/Main.java
@@ -18,29 +18,42 @@ public class Main {
         Collections.shuffle(contributors, new Random());
         Collections.shuffle(projects, new Random());
 
-        //        contributors.sort((c1, c2) -> Double.compare(c2.getCombinedScore(), c1.getCombinedScore()));
-//
+        // contributors.sort((c1, c2) -> Double.compare(c2.getCombinedScore(),
+        // c1.getCombinedScore()));
+        //
 
-        /** This sort is used for the c instance (collaborations) - it gives the score: 116985 **/
-//         Sort the projects by score (desc) and bestBefore (asc)
-//        projects.sort((p1, p2) -> {
-//            // Compare by score
-//            int scoreComparison = Integer.compare(p2.getScore(), p1.getScore());
-//            if (scoreComparison != 0) {
-//                return scoreComparison;
-//            }
-//            // Scores are equal, compare by bestBefore
-//            return Integer.compare(p1.getBestBefore(), p2.getBestBefore());
-//        });
+        /**
+         * This sort is used for the c instance (collaborations) - it gives the score:
+         * 116985
+         **/
+        // Sort the projects by score (desc) and bestBefore (asc)
+        // projects.sort((p1, p2) -> {
+        // // Compare by score
+        // int scoreComparison = Integer.compare(p2.getScore(), p1.getScore());
+        // if (scoreComparison != 0) {
+        // return scoreComparison;
+        // }
+        // // Scores are equal, compare by bestBefore
+        // return Integer.compare(p1.getBestBefore(), p2.getBestBefore());
+        // });
 
-        /** This sort is used for the e instance (exceptional skills) - it gives the score: 1551509  **/
-//         projects.sort((p1, p2) -> Double.compare(p2.getPriorityScore(), p1.getPriorityScore()));
+        /**
+         * This sort is used for the e instance (exceptional skills) - it gives the
+         * score: 1551509
+         **/
+        // projects.sort((p1, p2) -> Double.compare(p2.getPriorityScore(),
+        // p1.getPriorityScore()));
 
-        /** This sort is used for the f instance (great mentors) - it gives the score: 176150 **/
-//        projects.sort(Comparator.comparing(Project::getSkillLevels));
+        /**
+         * This sort is used for the f instance (great mentors) - it gives the score:
+         * 176150
+         **/
+        // projects.sort(Comparator.comparing(Project::getSkillLevels));
 
-        List<Contributor> firstCopyOfContributors = contributors.stream().map(Contributor::deepCopy).collect(Collectors.toList());
-        List<Contributor> secondCopyOfContributors = contributors.stream().map(Contributor::deepCopy).collect(Collectors.toList());
+        List<Contributor> firstCopyOfContributors = contributors.stream().map(Contributor::deepCopy)
+                .collect(Collectors.toList());
+        List<Contributor> secondCopyOfContributors = contributors.stream().map(Contributor::deepCopy)
+                .collect(Collectors.toList());
         List<Project> firstCopyOfProjects = projects.stream().map(Project::deepCopy).collect(Collectors.toList());
         List<Project> secondCopyOfProjects = projects.stream().map(Project::deepCopy).collect(Collectors.toList());
 
@@ -49,28 +62,19 @@ public class Main {
         System.out.println("Initial solution fitness score: " + fitnessScore);
         System.out.println("Initial assignments: " + assignments.size());
 
-        assignments = IteratedLocalSearch.iteratedLocalSearchWithRandomRestarts(assignments, Integer.parseInt(args[1]), projects, contributors);
+        assignments = IteratedLocalSearch.iteratedLocalSearchWithRandomRestarts(assignments, Integer.parseInt(args[1]),
+                projects, contributors);
 
         if (!Validator.areAssignmentsValid(assignments, firstCopyOfContributors, firstCopyOfProjects)) {
             System.out.println("Wrong initial solution");
             System.exit(0);
         }
 
-        int initialSolutionFitnessScore = FitnessCalculator.getFitnessScore(assignments);
-        System.out.println("Initial solution fitness score: " + initialSolutionFitnessScore);
+        fitnessScore = FitnessCalculator.getFitnessScore(assignments);
+        System.out.println("Final solution fitness score: " + fitnessScore);
+        System.out.println("Final assignments: " + assignments.size());
 
-        List<Assignment> changed_assignments = IteratedLocalSearch.iteratedLocalSearchWithRandomRestarts(assignments, 1, projects, contributors);
-
-        if (!Validator.areAssignmentsValid(changed_assignments, firstCopyOfContributors, firstCopyOfProjects)) {
-            System.out.println("Wrong initial solution");
-            System.exit(0);
-        }
-
-        initialSolutionFitnessScore = FitnessCalculator.getFitnessScore(changed_assignments);
-        System.out.println("Initial solution fitness score: " + initialSolutionFitnessScore);
-
-
-        OutputWriter.writeContent(changed_assignments, fileNames.get(1));
+        OutputWriter.writeContent(assignments, fileNames.get(1));
 
         List<NameAssignment> nameAssignments = InputReader.readAssignments(fileNames.get(1));
         List<Assignment> _assignments = new ArrayList<>();
@@ -100,4 +104,5 @@ public class Main {
             System.out.println("Wrong solution!");
         }
     }
+
 }
